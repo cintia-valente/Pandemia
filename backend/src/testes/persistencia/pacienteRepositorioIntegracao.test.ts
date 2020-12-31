87a94a46e71cd31034a42f1743923ce8eabc1b26
6381371-cintia-valente@users.noreply.gitlab.com <cintiavalente@live.com>
2020-12-31 20:03:28 -0300

    Começando os testes de integração de pacienteRepositorio

---
+++ backend/src/testes/persistencia/pacienteRepositorioIntegracao.test.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { PacienteRepositorio } from '../../persistencia/repositorio/pacienteRepositorio';
+import * as dbhandler from '../dbhandler';
+import { Paciente } from '../../entidades/paciente';
+import { PacienteModel } from '../../persistencia/model/pacienteModel';
+import app from '../../app';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
+
+/**
+ * Conecta com o banco de dados antes de executar qualquer teste.
+ */
+beforeAll(async () => {
+    await dbhandler.connect();
+});
+
+/**
+ * Limpa os dados após cada teste.
+ */
+afterEach(async () => {
+    await dbhandler.clearDatabase();
+});
+
+/**
+ * Remove e fecha a conexão com o banco de dados após os teste.
+ */
+afterAll(async () => {
+    await dbhandler.closeDatabase();
+});
